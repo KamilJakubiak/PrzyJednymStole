2ad90fc357fa513adbe97db093566628c1ad6e35
Kamil Jakubiak <hellokamil@proton.me>
2026-06-05 00:30:47 +0200

    Delete assets directory

--- assets/nbspify.js
+++
@@ -1,7 +0,0 @@
-function nbspify(str) {
-  if (!str) return str;
-  return str.replace(
-    /(^|\s)([aioüzżw]|do|na|po|we|ze|od|ku|za|bo|by|co|go|to|że|już|jak|ale|czy|lub|ani|nie|tak)\s/gi,
-    function(_, pre, word) { return pre + word + ' '; }
-  );
-}
